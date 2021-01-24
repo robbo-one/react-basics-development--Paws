d6a9d2e685516335500fabeaca9fe7181e5f01bd
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-25 10:47:48 +1300

    adding cheese

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Dog from './Dog.jsx'
+import Subtitle from './Subtitle.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Subtitle text='He like cheese.' />
     <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' />
+    <Subtitle text='He like cheese.' />
     <Dog name='Chloe' breed='Jack-russel schnauzer' superpower='Sausage detection'/>
+    <Subtitle text='She dont like cheese.' />
     <Dog name='Naga' breed='Tabby' superpower='Chonker' />
   </div>
 )
