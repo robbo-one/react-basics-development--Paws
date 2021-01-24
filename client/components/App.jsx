e5d8b62e34f7779c1e31c265d4434072bcb5d1fa
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-25 10:38:16 +1300

    init commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Dog from './Dog.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision'/>
   </div>
 )
 
