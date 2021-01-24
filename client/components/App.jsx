7cdfa4dce22b4cbd35c39cceb5f89c1bc4c32126
student <student@eda-bc-09.localdomain>
2021-01-25 10:39:10 +1300

    adding new dogs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Dog from './Dog.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' />
+    <Dog name='Chloe' breed='Jack-russel schnauzer' superpower='Sausage detection'/>
+    <Dog name='Naga' breed='Tabby' superpower='Chonker' />
   </div>
 )
 
