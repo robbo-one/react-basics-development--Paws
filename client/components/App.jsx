046c5db7dc7928848916a375e72e87866474c257
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-01-25 10:52:33 +1300

    Define Subtitle component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Dog from './Dog.jsx'
+import Subtitle from "./Subtitle.jsx"
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Subtitle text='Canines using supercanine abilities for social good.' />
     <Dog name='savannah' breed='huntaway' superpower='detroying the damn couch' />
   </div>
 )
---
+++ client/components/Subtitle.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Subtitle = (props) => {
+  return (
+    <h2>
+      {props.text}
+    </h2>
+  )
+}
+
+export default Subtitle
