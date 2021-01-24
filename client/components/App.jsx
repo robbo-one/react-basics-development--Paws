27b5d806fcb6206098ff42e14a56e250d8ce941a
Dylan Hodges <73857570+dylan-hodges313@users.noreply.github.com>
2021-01-25 10:42:37 +1300

    add a dog

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Dog from './Dog.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Dog name='savannah' breed='huntaway' superpower='detroying the damn couch' />
   </div>
 )
 
