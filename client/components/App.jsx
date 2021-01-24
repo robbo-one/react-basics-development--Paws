1a12f436a164124ac93c01671f818c0afe153acb
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-25 10:41:31 +1300

    Task 1 completed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Dog from './Dog.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' />
   </div>
 )
+
+// ReactDOM.render(
+//   <Dog name={data.name}/>, 
+//   document.getElementById('root')
+// )
+
 
 export default App
