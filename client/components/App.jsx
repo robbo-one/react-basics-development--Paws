8b530c3d0774600971869c9229c6ff6db6b51c28
Samardeep90 <73861988+Samardeep90@users.noreply.github.com>
2021-01-25 11:13:02 +1300

    Add subtitle

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Dog from './Dog.jsx'
+import Subtitle from './Subtitle.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <h2>
+    <Subtitle text='Canines using supercanine abilities for social good.' />
+    </h2>
     <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' />
+    
   </div>
 )
 
-// ReactDOM.render(
-//   <Dog name={data.name}/>, 
-//   document.getElementById('root')
-// )
-
 
 export default App
+  
+ 
---
+++ client/components/Subtitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Subtitle = (props) => {
+  return (
+    <div className='subtitle-wrapper'>
+       <span className='subtitle-text'>{props.text}</span>
+      </div>
+      )
+ }
+ 
+
+export default Subtitle
